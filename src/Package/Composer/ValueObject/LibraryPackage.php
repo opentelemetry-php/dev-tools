d1f09c75ed96ce3b23335cb5ad9ada5baf9ed3f2
Timo Michna <timomichna@yahoo.de>
2022-05-31 13:59:36 +0200

    Add LibraryPackage

---
+++ src/Package/Composer/ValueObject/LibraryPackage.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\DevTools\Package\Composer\ValueObject;
+
+use OpenTelemetry\DevTools\Package\Composer\PackageTypes;
+
+class LibraryPackage implements PackageInterface
+{
+    use PackageTrait;
+
+    private const TYPE_VALUE = 'path';
+
+    public function __construct(string $name)
+    {
+        $this->name = $name;
+    }
+
+    public static function create(string $path): self
+    {
+        return new self($path);
+    }
+
+    public function getType(): string
+    {
+        return PackageTypes::LIBRARY_TYPE;
+    }
+}
