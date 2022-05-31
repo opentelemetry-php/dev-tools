1c5e575bc50eae0dbfcab31f14e55b915c2bd80b
Timo Michna <timomichna@yahoo.de>
2022-05-31 14:31:10 +0200

    Add LocalRepository

---
+++ src/Package/Composer/ValueObject/LocalRepository.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\DevTools\Package\Composer\ValueObject;
+
+use OpenTelemetry\DevTools\Package\Composer\RepositoryTypes;
+
+class LocalRepository implements SingleRepositoryInterface
+{
+    use SingleRepositoryTrait;
+
+    protected function setType(): void
+    {
+        $this->type = RepositoryTypes::PATH_TYPE;
+    }
+}
---
+++ tests/Unit/Package/Composer/ValueObject/LocalRepositoryTest.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\DevTools\Tests\Unit\Package\Composer\ValueObject;
+
+use OpenTelemetry\DevTools\Package\Composer\RepositoryTypes;
+use OpenTelemetry\DevTools\Package\Composer\ValueObject\LocalRepository;
+
+/**
+ * @covers \OpenTelemetry\DevTools\Package\Composer\ValueObject\LocalRepository
+ */
+class LocalRepositoryTest extends AbstractRepositoryTraitTest
+{
+    public function test_get_type(): void
+    {
+        $this->assertSame(
+            RepositoryTypes::PATH_TYPE,
+            LocalRepository::create(
+                'foo',
+                $this->createPackageInterfaceMock()
+            )->getType()
+        );
+    }
+}
