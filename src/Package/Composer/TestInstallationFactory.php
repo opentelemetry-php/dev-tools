28527b6a362523b93389b5331c16f42eb1cfef86
Timo Michna <timomichna@yahoo.de>
2022-06-01 13:53:19 +0200

    Add TestInstallationFactory

---
+++ src/Package/Composer/TestInstallationFactory.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OpenTelemetry\DevTools\Package\Composer;
+
+class TestInstallationFactory
+{
+    private array $defaultDependencies = [];
+
+    public function __construct(array $defaultDependencies = [])
+    {
+        foreach ($defaultDependencies as $packageName => $versionConstraint) {
+            $this->addDefaultDependency(
+                $packageName,
+                $versionConstraint
+            );
+        }
+    }
+
+    public static function create(array $defaultDependencies = []): self
+    {
+        return new self($defaultDependencies);
+    }
+
+    public function build(?string $name = null, ?string $type = null): TestInstallation
+    {
+        $installation = TestInstallation::create($name, $type);
+
+        foreach ($this->defaultDependencies as $packageName => $versionConstraint) {
+            $installation->addRequire($packageName, $versionConstraint);
+        }
+
+        return $installation;
+    }
+
+    public function addDefaultDependency(string $packageName, string $versionConstraint): void
+    {
+        $this->defaultDependencies[$packageName] = $versionConstraint;
+    }
+}
