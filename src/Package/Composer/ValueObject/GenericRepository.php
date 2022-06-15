20cbd0085d8d77a641a8fb2b1b156ff32b9085dd
Timo Michna <timomichna@yahoo.de>
2022-06-15 15:17:51 +0200

    Add MultiRepositoryInterface

--- src/Package/Composer/ValueObject/GenericRepository.php
+++ src/Package/Composer/ValueObject/GenericRepository.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace OpenTelemetry\DevTools\Package\Composer\ValueObject;
 
-class GenericRepository implements RepositoryInterface
+class GenericRepository implements MultiRepositoryInterface
 {
     use RepositoryTrait;
 
